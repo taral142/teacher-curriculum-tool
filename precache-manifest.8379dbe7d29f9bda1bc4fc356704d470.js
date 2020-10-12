c4b040f69b853c201af35f132b6a940aac89945b
taral142 <taraltaral1212@gmail.com>
2020-10-12 08:56:27 -0700

    Updates

---
+++ precache-manifest.8379dbe7d29f9bda1bc4fc356704d470.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7a424da32f134d4c8d220e4d2eeaea",
+    "url": "/teacher-curriculum-tool/index.html"
+  },
+  {
+    "revision": "0a5c6ac447b07ba27d06",
+    "url": "/teacher-curriculum-tool/static/css/main.3e26cfa1.chunk.css"
+  },
+  {
+    "revision": "2b0cc945fcdba05c5579",
+    "url": "/teacher-curriculum-tool/static/js/2.1342e7e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/teacher-curriculum-tool/static/js/2.1342e7e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a5c6ac447b07ba27d06",
+    "url": "/teacher-curriculum-tool/static/js/main.9bf589a4.chunk.js"
+  },
+  {
+    "revision": "f9b44b056b6f1f8ebd25",
+    "url": "/teacher-curriculum-tool/static/js/runtime-main.98b149e4.js"
+  }
+]);
